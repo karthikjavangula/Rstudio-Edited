a8c9c915acd4ae121588d71abaa11189b40275ae
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-05-26 23:44:00 +0530

    Update BST.c

--- BST.c
+++ BST.c
@@ -1,156 +1,102 @@
-#include<stdio.h> 
-#include<stdlib.h> 
-
-struct node {
-    int key;
-    struct node *left, *right;
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
 };
-
-struct node* newNode(int item) {
-    struct node *temp = (struct node*)malloc(sizeof(struct node));
-    temp->key = item;
-    temp->left = NULL;
-    temp->right = NULL;
-    return temp;
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
 }
-
-void inorder(struct node *root) {
-    if(root != NULL) {
-        inorder(root->left);
-        printf("%d -> ", root->key);
-        inorder(root->right);
+struct Node* insertNode(struct Node* root, int value) {
+    if (root == NULL) {
+        return createNode(value);
     }
-}
-
-struct node *insert(struct node *node, int key) {
-    if(node == NULL) return newNode(key);
-    if(key < node->key)
-        node->left = insert(node->left, key);
-    else
-        node->right = insert(node->right, key);
-    return node;
-}
-
-struct node *deleteNode(struct node *root, int key) {
-    if(root == NULL) return root;
-    if(key < root->key)
-        root->left = deleteNode(root->left, key);
-    else if(key > root->key)
-        root->right = deleteNode(root->right, key);
-    else {
-        if(root->left == NULL) {
-            struct node *temp = root->right;
-            free(root);
-            return temp;
-        }
-        else if(root->right == NULL) {
-            struct node *temp = root->left;
-            free(root);
-            return temp;
-        }
-        struct node *temp = minValueNode(root->right);
-        root->key = temp->key;
-        root->right = deleteNode(root->right, temp->key);
+    if (value < root->data) {
+        root->left = insertNode(root->left, value);
+    } else if (value > root->data) {
+        root->right = insertNode(root->right, value);
     }
+
     return root;
 }
-
-struct node *minValueNode(struct node *node) {
-    struct node *current = node;
-    while(current && current->left != NULL) {
+void inOrderTraversal(struct Node* root) {
+    if (root != NULL) {
+        inOrderTraversal(root->left);
+        printf("%d ", root->data);
+        inOrderTraversal(root->right);
+    }
+}
+void preOrderTraversal(struct Node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preOrderTraversal(root->left);
+        preOrderTraversal(root->right);
+    }
+}
+void postOrderTraversal(struct Node* root) {
+    if (root != NULL) {
+        postOrderTraversal(root->left);
+        postOrderTraversal(root->right);
+        printf("%d ", root->data);
+    }
+}
+struct Node* findMinValueNode(struct Node* root) {
+    struct Node* current = root;
+    while (current && current->left != NULL) {
         current = current->left;
     }
     return current;
 }
-
+struct Node* deleteNode(struct Node* root, int value) {
+    if (root == NULL) {
+        return root;
+    }
+    if (value < root->data) {
+        root->left = deleteNode(root->left, value);
+    } else if (value > root->data) {
+        root->right = deleteNode(root->right, value);
+    } else {
+        if (root->left == NULL) {
+            struct Node* temp = root->right;
+            free(root);
+            return temp;
+        } else if (root->right == NULL) {
+            struct Node* temp = root->left;
+            free(root);
+            return temp;
+        }
+        struct Node* minValueNode = findMinValueNode(root->right);
+        root->data = minValueNode->data;
+        root->right = deleteNode(root->right, minValueNode->data);
+    }
+    return root;
+}
 int main() {
-    struct node *root = NULL;
-    root = insert(root, 8);
-    root = insert(root, 3);
-    root = insert(root, 1);
-    root = insert(root, 6);
-    root = insert(root, 7);
-    root = insert(root, 10);
-    root = insert(root, 14);
-    root = insert(root, 4);
-
-    printf("Inorder Traversal: ");
-    inorder(root);
-    
-    printf("\nDeleting a node\n");
-    root = deleteNode(root, 10);
-    printf("Inorder Traversal: ");
-    inorder(root);
-
+    struct Node* root = NULL;
+    root = insertNode(root, 50);
+    root = insertNode(root, 30);
+    root = insertNode(root, 20);
+    root = insertNode(root, 40);
+    root = insertNode(root, 70);
+    root = insertNode(root, 60);
+    root = insertNode(root, 80);
+    printf("In-order traversal: ");
+    inOrderTraversal(root);
+    printf("\n");
+    printf("Pre-order traversal: ");
+    preOrderTraversal(root);
+    printf("\n");
+    printf("Post-order traversal: ");
+    postOrderTraversal(root);
+    printf("\n");
+    root = deleteNode(root, 40);
+    printf("In-order traversal after deletion: ");
+    inOrderTraversal(root);
+    printf("\n");
     return 0;
-}
-
-
-void inorder(struct node *root){
-    if(root!=NULL){
-        inorder(root->left);
-        printf("%d -> ",root->key);
-        inorder(root->right);
-    }
-}
-
-/*void preorder(struct node *root){
-    if(root!=NULL){
-        printf("%d -> ",root->key);
-        preorder(root->left);
-        preorder(root->right);
-    }
-}
-
-void postorder(struct node *root){
-    if(root!=NULL){
-        postorder(root->left);
-        postorder(root->right);
-        printf("%d -> ",root->key);
-    }
-}*/
-
-struct node *insert(struct node *node,int key){
-    if(node=NULL) return newNode(key);
-    if(key < node->key)
-        node->left = insert(node->left,key);
-    else
-        node->right = insert(node->right,key);
-    return node;
-}
-
-struct node *deleteNode(struct node *root,int key){
-    if(root == NULL) return NULL;
-    if(key < root->key){
-        root->left = deleteNode(root->left,key);
-    }
-    else if(key > root->key){
-        root->right = deleteNode(root->right,key);
-    }
-    else{
-        if(root->left == NULL){
-            struct node *temp = root->right;
-            free(root);
-            return temp;
-        }
-        else if(root->right == NULL){
-            struct node *temp = root->left;
-            free(root);
-            return temp;
-        }
-        else{
-        struct node *temp = minValueNode(root->right);
-        root->key = temp->key;
-        root->right = deleteNode(root->right,temp->key);
-        }
-    }
-    return root;
-}
-
-struct node *minValueNode(struct node *node){
-    struct node *current = node;
-    while(current && current->left != NULL){
-        current = current->left;
-    }
-    return current;
 }
